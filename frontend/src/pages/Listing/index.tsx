1931726122cecb2487626ff91578eaef8d86bfd6
eliseunetto <eliseunetto@hotmail.com>
2022-01-11 21:16:17 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,33 @@
 import Pagination from "components/Pagination";
+import MovieCard from "../../components/MovieCard/index";
 import "./styles.css";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
